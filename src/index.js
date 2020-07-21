2c93f2d3f96a05b61a9e4e2c25fc1c7d5df8bf50
ashish-netizen <rajawatashish23@gmail.com>
2020-07-21 22:42:22 +0530

    Greeted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+let cd= new Date( );
+cd= cd.getHours();
+let greet= "";
+const cssStyle= {};
+
+if (cd>=1 && cd<12)
+{
+greet= "Good Morning";
+cssStyle.color="Green"
+} 
+else if(cd>=12 && cd<19)
+{
+
+  greet= " Good Evening";
+  cssStyle.color="Orange"
+
+}
+else{
+  greet= "Good Night";
+  cssStyle.color="Blue"
+
+}
+
+
+
+
+ReactDom.render(
+  <div>
+  
+<div className= "mid"><h1>Hello there, <span style= {cssStyle}> {greet} </span></h1></div>
+</div>,
+  document.getElementById("root")
+)
+
